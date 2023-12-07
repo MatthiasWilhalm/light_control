43af05c287786529016f6a2faa81536a95b89ac0
MatthiasWilhalm <matthias.wilhalm@gmail.com>
2023-12-07 19:13:10 +0100

    clean up and add better error handling

--- server/server.py
+++ server/server.py
@@ -1,18 +1,32 @@
 import serial
+import atexit
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+# start REST API server
 app = Flask(__name__)
 CORS(app)
-ser = serial.serial_for_url('rfc2217://localhost:4000', baudrate=9600)
 
-print("connected to serial connection: " + ser.name)
+# start serial connection
+ser = None
 
-# api endpoints
+def cleanup():
+    if ser:
+        ser.close()
+        print("Serial connection closed")
 
-@app.route('/hello', methods=['GET'])
-def api_endpoint():
-    return jsonify(message="Hello, World!")
+atexit.register(cleanup)
+
+try:
+    # use this for the emulator
+    # ser = serial.serial_for_url('rfc2217://localhost:4000', baudrate=9600)
+    ser = serial.Serial(port='COM3', baudrate=9600)
+    print("connected to serial connection: " + ser.name)
+except:
+    print("failed to connect to serial connection... exiting")
+    exit()
+
+### api endpoints ###
 
 # accepts a JSON object with a light_index number property
 @app.route('/setlight', methods=['POST'])
@@ -23,31 +37,35 @@ def post_endpoint():
     
     light_index = j_data['received_data']['index']
     light_state = 1 if j_data['received_data']['state'] == True else 0
-    serial_msg = str(light_index) + "," + str(light_state)
-    
-    res = str(serial_msg).encode('utf-8')
-    print("sending data via serial: " + res.decode('utf-8'))
-    ser.write(res)
+    send_serial_msg(str(light_index) + "," + str(light_state))
+        
     return jsonify(message="Data received", received_data=data)
 
+# resets all lights to off
 @app.route('/reset', methods=['GET'])
 def reset_endpoint():
-    ser.write(b"reset")
+    send_serial_msg("reset")
     return jsonify(message="Resetting all lights")
 
+# sets all lights at once
 @app.route('/setall', methods=['POST'])
 def setall_endpoint():
     data = request.get_json()
     j_data = jsonify(message="Data received", received_data=data).json
     print("received data: " + str(j_data['received_data']))
     
-    path = j_data['received_data']['path']
-    serial_msg = "set" + str(path)
+    path = j_data['received_data']['path']    
+    send_serial_msg("set" + str(path))
     
-    res = str(serial_msg).encode('utf-8')
-    print("sending data via serial: " + res.decode('utf-8'))
-    ser.write(res)
     return jsonify(message="Data received", received_data=data)
 
+### helper functions ###
+
+# sends a message to the serial connection
+def send_serial_msg(msg):
+    res = str(msg).encode('utf-8')
+    print("sending data via serial: " + res.decode('utf-8'))
+    ser.write(res)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+void setLed(uint8_t ledIndex, uint8_t state);
+void setAllLeds(String ledsToSet);
 void reset();
 
 const uint8_t ledPins[] = {3, 4, 5, 6, 7, 8, 9, 10};
@@ -20,6 +22,7 @@ void loop() {
 
   // Commands
 
+  // reset
   if(text.equals("reset")) {
     reset();
     return;
@@ -27,42 +30,60 @@ void loop() {
 
   // set:00000000 (8 bits, 0 = off, 1 = on)
   // to set all LEDs at once
-
   if(text.startsWith("set")) {
     String ledsToSet = text.substring(3);
-    for (uint8_t i = 0; i < sizeof(ledPins); i++) {
-      if(ledsToSet[i] == '1') {
-        digitalWrite(ledPins[i], HIGH);
-      } else {
-        digitalWrite(ledPins[i], LOW);
-      }
-    }
+    setAllLeds(ledsToSet);
     return;
   }
 
   // default single LED control
-
   int ledIndex = text.substring(0, text.indexOf(',')).toInt();
   int ledState = text.substring(text.indexOf(',') + 1).toInt(); // 0 = off, 1 = on
-  
+  setLed(ledIndex, ledState);
+}
+
+/**
+ * @brief Sets a single LED.
+ * 
+ * @param ledIndex 0-7
+ * @param state 0 = off, 1 = on
+ */
+void setLed(uint8_t ledIndex, uint8_t state) {
   if(ledIndex > 7 || ledIndex < 0) {
     Serial.println("Invalid LED index ("+String(ledIndex)+")");
     return;
   }
-
-  if(ledState == 0) {
+  if(state == LOW) {
     Serial.println("Turning off LED "+String(ledIndex));
-    digitalWrite(ledPins[ledIndex], LOW);
-    return;
-  } 
-  if(ledState == 1) {
+  } else if (state == HIGH) {
     Serial.println("Turning on LED "+String(ledIndex));
-    digitalWrite(ledPins[ledIndex], HIGH);
+  } else {
+    Serial.println("Invalid LED state ("+String(state)+")");
     return;
   }
-  Serial.println("Invalid LED state ("+String(ledState)+")");
+  digitalWrite(ledPins[ledIndex], state);
 }
 
+/**
+ * @brief Sets all LEDs at once.
+ * format: ledIndex,ledState (e.g. 0,1 = turn on LED 0)
+ * @param ledsToSet 8 bits as string, 0 = off, 1 = on
+ */
+void setAllLeds(String ledsToSet) {
+  for (uint8_t i = 0; i < sizeof(ledPins); i++) {
+    if(ledsToSet[i] == '1') {
+      digitalWrite(ledPins[i], HIGH);
+    } else {
+      digitalWrite(ledPins[i], LOW);
+    }
+  }
+  
+}
+
+/**
+ * @brief Turns off all LEDs.
+ * 
+ */
 void reset() {
   for (uint8_t i = 0; i < sizeof(ledPins); i++) {
     digitalWrite(ledPins[i], LOW);
