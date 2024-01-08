a7af8757fdb913adbe11600bae34273749ddcfcc
Matthias Wilhalm <matthias.wilhalm@gmail.com>
2024-01-08 17:17:33 +0100

    remove rest code

--- server/server.py
+++ server/server.py
@@ -7,19 +7,12 @@ import socket
 from tcp_server import TCPServer
 from websocket_server import WebSocketServer
 
-from flask import Flask, jsonify, request
-from flask_cors import CORS
-
 TCP_ADDRESS = 'localhost'
 TCP_PORT = 12345
 SERIAL_PORT = 'COM3'
-REST_PORT = 5000
 WEBSOCKET_ADDRESS = 'localhost'
 WEBSOCKET_PORT = 8765
 
-# REST API server
-app = Flask(__name__)
-CORS(app)
 
 global serial_connection
 global tcp_server_thread
@@ -29,7 +22,6 @@ global stop_event
 
 serial_connection = None
 tcp_server_thread = None
-flask_server_thread = None
 websocket_server_thread = None
 
 stop_event = threading.Event()
@@ -48,37 +40,6 @@ def cleanup():
         print("Websocket server thread closed")
 
 atexit.register(cleanup)
-
-# accepts a JSON object with a light_index number property
-@app.route('/setlight', methods=['POST'])
-def post_endpoint():
-    data = request.get_json()
-    j_data = jsonify(message="Data received", received_data=data).json
-    print("received data: " + str(j_data['received_data']))
-    
-    light_index = j_data['received_data']['index']
-    light_state = 1 if j_data['received_data']['state'] == True else 0
-    send_serial_msg(str(light_index) + "," + str(light_state))
-        
-    return jsonify(message="Data received", received_data=data)
-
-# resets all lights to off
-@app.route('/reset', methods=['GET'])
-def reset_endpoint():
-    send_serial_msg("reset")
-    return jsonify(message="Resetting all lights")
-
-# sets all lights at once
-@app.route('/setall', methods=['POST'])
-def setall_endpoint():
-    data = request.get_json()
-    j_data = jsonify(message="Data received", received_data=data).json
-    print("received data: " + str(j_data['received_data']))
-    
-    path = j_data['received_data']['path']    
-    send_serial_msg("set" + str(path))
-    
-    return jsonify(message="Data received", received_data=data)
 
 ### helper functions ###
 
@@ -105,8 +66,8 @@ if __name__ == '__main__':
     websocket_server_thread.start()
 
     try:
-        app.run()
-        print("REST API server started on port " + str(REST_PORT))
+        while True:
+            time.sleep(1)
     except KeyboardInterrupt:
         print("exiting...")
         cleanup()
--- web/main.js
+++ web/main.js
@@ -15,15 +15,13 @@ Array.from(document.getElementsByTagName('path')).forEach((light, i) => {
     light.addEventListener('click', () => {
         const nextState = !light.classList.contains('selected');
         updateDisplayLight(light, nextState);
-        // sendLightUpdate(i, nextState);
-        sendLightUpdateWS(i, nextState);
+        sendLightUpdate(i, nextState);
     });
 });
 
 document.getElementById('reset').addEventListener('click', () => {
     updateDisplayByPath([]);
-    // sendReset();
-    sendResetWS();
+    sendReset();
 });
 
 document.getElementById('random').addEventListener('click', () => {
@@ -33,14 +31,13 @@ document.getElementById('random').addEventListener('click', () => {
 document.getElementById('all_on').addEventListener('click', () => {
     const path = new Array(24).fill(0).map((_, i) => i);
     updateDisplayByPath(path);
-    // sendPath(path);
-    sendPathWS(path);
+    sendPath(path);
 });
 
 document.getElementById('send').addEventListener('click', () => {
     const path = new Array(24).fill(0).map((_, i) => i);
     updateDisplayByPath(path);
-    sendPathWS(path);
+    sendPath(path);
 });
 
 /**
@@ -50,7 +47,7 @@ const sendRandomPath = () => {
     const path = calcRandomPath();
     updateDisplayByPath(path);
     // sendPath(path);
-    sendPathWS(path);
+    sendPath(path);
 };
 
 /**
@@ -86,58 +83,6 @@ const updateDisplayByPath = (paths) => {
  * @param {boolean} state
  */
 const sendLightUpdate = (index, state) => {
-    fetch('http://localhost:5000/setlight', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        body: JSON.stringify({
-            index: index,
-            state: state
-        })
-    }).then(response => {
-        if(response.status !== 200)
-            console.log(response);
-    });
-}
-
-/**
- * sends a request to the server to reset all lights
- */
-const sendReset = () => {
-    fetch('http://localhost:5000/reset', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-    }).then(response => {
-        if(response.status !== 200)
-            console.log(response);
-    });
-}
-
-/**
- * @param {number[]} path [0, 1, 2, 3, 4] (light indexes)
- */
-const sendPath = (path) => {
-    fetch('http://localhost:5000/setall', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        mode: 'cors',
-        body: JSON.stringify({
-            path: new Array(24).fill(0).map((_, i) => path.includes(i) ? '1' : '0').join('')
-        })
-    }).then(response => {
-        if(response.status !== 200)
-            console.log(response);
-    });
-}
-
-const sendLightUpdateWS = (index, state) => {
     socket.send(JSON.stringify({
         path: '/setlight',
         body: {
@@ -147,14 +92,20 @@ const sendLightUpdateWS = (index, state) => {
     }));
 }
 
-const sendResetWS = () => {
+/**
+ * sends a request to the server to reset all lights
+*/
+const sendReset = () => {
     socket.send(JSON.stringify({
         path: '/reset',
         body: ''
     }));
 }
 
-const sendPathWS = (path) => {
+/**
+ * @param {number[]} path [0, 1, 2, 3, 4] (light indexes)
+*/
+const sendPath = (path) => {
     socket.send(JSON.stringify({
         path: '/setall',
         body: new Array(24).fill(0).map((_, i) => path.includes(i) ? '1' : '0').join('')
